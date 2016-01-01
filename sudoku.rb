89c94cc0789cacfe62fc975dde98f4d759598aa1
David Ma & Phil Thomas <pair+dma315.pthomas551@devbootcamp.com>
2016-01-01 14:13:50 -0600

    Add pretty board and board to string + tests

--- sudoku.rb
+++ sudoku.rb
@@ -18,6 +18,17 @@ end
 #The provided board will be in whatever
 #form `solve` returns.
 def pretty_board(board)
+  pretty_board = ''
+  board.each_slice(9) do |row|
+    row.each_with_index do |cell, index|
+      pretty_row = ''
+      if cell.length > 1
+        pretty_row += row[index] = '-'
+      end
+    end
+  pretty_board += row.join('  ') + "\n"
+  end
+  pretty_board
 end
 
 #Takes in a board in some form and
@@ -26,4 +37,28 @@ end
 #The provided board will be in whatever
 #form `solve` returns.
 def board_to_string(board)
+  string_board = ''
+  board.each do |cell|
+    if cell.length > 1
+      string_board << "-"
+    else
+      string_board << cell.join("")
+    end
+  end
+  string_board
 end
+
+test_board = [
+  [1],[2],[3],[4],[5],[6],[7],[8],[9],
+  [1],[2,4],[3],[4],[5],[6],[7],[8],[9],
+  [1],[2],[3,5],[4],[5],[6],[7],[8],[9],
+  [1],[2],[3],[4,7],[5],[6],[7],[8],[9],
+  [1],[2],[3],[4],[5,8],[6],[7],[8],[9],
+  [1],[2],[3],[4],[5],[6,9],[7],[8],[9],
+  [1],[2],[3],[4],[5],[6],[7,9,1,3],[8],[9],
+  [1],[2],[3],[4],[5],[6],[7],[5,6],[9],
+  [1],[2],[3],[4],[5],[6],[7],[8],[9,5]
+]
+
+p pretty_board(test_board)
+p board_to_string(test_board)
--- sudoku_spec.rb
+++ sudoku_spec.rb
@@ -1,2 +1,31 @@
 require_relative 'sudoku'
 
+describe 'sudoku' do
+    let(:board_array) {[
+                      [1],[2],[3],[4],[5],[6],[7],[8],[9],
+                      [1],[2,4],[3],[4],[5],[6],[7],[8],[9],
+                      [1],[2],[3,5],[4],[5],[6],[7],[8],[9],
+                      [1],[2],[3],[4,7],[5],[6],[7],[8],[9],
+                      [1],[2],[3],[4],[5,8],[6],[7],[8],[9],
+                      [1],[2],[3],[4],[5],[6,9],[7],[8],[9],
+                      [1],[2],[3],[4],[5],[6],[7,9,1,3],[8],[9],
+                      [1],[2],[3],[4],[5],[6],[7],[5,6],[9],
+                      [1],[2],[3],[4],[5],[6],[7],[8],[9,5] ]}
+
+  describe 'pretty_board' do
+    it 'prints a prettily formatted sudoku board to the screen' do
+      expect(pretty_board(board_array)).to eq("1  2  3  4  5  6  7  8  9\n1  -  3  4  5  6  7  8  9\n1  2  -  4  5  6  7  8  9\n1  2  3  -  5  6  7  8  9\n1  2  3  4  -  6  7  8  9\n1  2  3  4  5  -  7  8  9\n1  2  3  4  5  6  -  8  9\n1  2  3  4  5  6  7  -  9\n1  2  3  4  5  6  7  8  -\n")
+    end
+  end
+
+  describe 'board_to_string' do
+    it 'takes a board array and returns a string in original format' do
+      expect(board_to_string(board_array)).to eq("1234567891-345678912-456789123-567891234-678912345-789123456-891234567-912345678-")
+    end
+
+    it 'returns a string of the proper length' do
+       expect(board_to_string(board_array).length).to eq (81)
+    end
+  end
+
+end
