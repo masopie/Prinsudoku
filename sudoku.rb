d9a302584d8a3f9786b4e877191db2aa887296d7
Alan Alcesto & David Diaz & Tom McHenry & Zollie Barnes <pair+aalcesto23.ddiaz914.tommchenry.zoltron2001@devbootcamp.com>
2016-01-01 19:09:57 -0600

    Finish Release 0 of sudoku challenge.

--- sudoku.rb
+++ sudoku.rb
@@ -2,7 +2,19 @@
 #you see in the puzzle file. Returns
 #something representing a board after
 #your solver has tried to solve it.
-def solve(board_string)
+def solve(string)
+  board = string_to_board(string)
+  while solved?(string) == false
+    board.each_with_index do |row, row_index|
+      row.each_with_index do |cell, col_index|
+        if cell == "-"
+          replace_value(board, row_index, col_index,check_cell(board,row_index,col_index))
+          string = board_to_string(board)
+        end
+      end
+    end
+  end
+  return board_to_string(board)
 end
 
 #Returns a boolean indicating whether
@@ -10,6 +22,7 @@ end
 #The provided board will be in whatever
 #form `solve` returns.
 def solved?(board)
+  board.include?("-") ? false : true
 end
 
 #Takes in a board in some form and
@@ -26,4 +39,94 @@ end
 #The provided board will be in whatever
 #form `solve` returns.
 def board_to_string(board)
+  board.flatten.join
 end
+
+def string_to_board(string)
+  string.split(//).each_slice(9).to_a
+end
+
+def make_row(array, row_number)
+  array[row_number]
+end
+
+def make_column(array, column_number)
+  column = []
+  array.each do |row|
+    column << row[column_number]
+  end
+  column
+end
+
+def square_range(row, column)
+  square_column1 = [0,3,6]
+  square_column2 = [1,4,7]
+  square_column3 = [2,5,8]
+  index = 0 if row >= 0 && row <= 2
+  index = 1 if row >= 3 && row <= 5
+  index = 2 if row >= 6 && row <= 8
+  array = square_column1 if column >= 0 && column <= 2
+  array = square_column2 if column >= 3 && column <= 5
+  array = square_column3 if column >= 6 && column <= 8
+  array[index]
+end
+
+def identify_and_make_square(array, square_number)
+  square_coor = {
+    0 => [0,0], 1 => [0,3], 2 => [0,6],
+    3 => [3,0], 4 => [3,3], 5 => [3,6],
+    6 => [6,0], 7 => [6,3], 8 => [6,6]
+  }
+  make_square(array, square_coor[square_number])
+end
+
+def make_square(array, array_of_coordinates)
+  square_array = []
+  row_start = array_of_coordinates[0]
+  row_end = row_start + 2
+  column_start = array_of_coordinates[1]
+  column_end = column_start + 2
+  counter = row_start
+  while counter <= row_end
+    square_array << array[counter][column_start..column_end]
+    counter += 1
+  end
+  square_array.flatten
+end
+
+def possible_values(array)
+  return_array = ["1","2","3","4","5","6","7","8","9"]
+  return_array_dup = return_array.dup
+  return_array_dup.each do |return_array_value|
+    if array.include?(return_array_value)
+      return_array.delete(return_array_value)
+    end
+  end
+  return_array.sort
+end
+
+def check_cell(board,row,column)
+  row_return_values = possible_values(make_row(board,row))
+  col_return_values = possible_values(make_column(board,column))
+  square_number = square_range(row,column)
+  square_return_values = possible_values(identify_and_make_square(board,square_number))
+  uniq_val = row_return_values & col_return_values & square_return_values
+  if uniq_val.length == 1
+    return uniq_val[0]
+  else
+    return false
+  end
+end
+
+def replace_value(board, row, column, answer)
+  if answer
+    board[row][column] = answer
+  end
+  board
+end
+
+solve("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--")
+
+# solve("--5-3--819-285--6-6----4-5---74-283-34976---5--83--49-15--87--2-9----6---26-495-3")
+
+
--- sudoku_spec.rb
+++ sudoku_spec.rb
@@ -1,2 +1,213 @@
 require_relative 'sudoku'
 
+describe "solved?" do
+  solved_puzzle = "1" * 81
+  blank_puzzle = "-" * 81
+  unsolved_puzzle = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+
+  it "return true if board is solved" do
+    expect(solved?(solved_puzzle)).to eq(true)
+  end
+
+  it "return false if board is empty" do
+    expect(solved?(blank_puzzle)).to eq(false)
+  end
+
+  it "returns false if board is unsolved" do
+    expect(solved?(unsolved_puzzle)).to eq(false)
+  end
+end
+
+describe "string_to_board" do
+  string = "1--1--1--1--1--1--1--1--1--"
+  unsolved_puzzle = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+  correct_board = [
+    ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+    ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+    ["2", "-", "-", "4", "-", "-", "8", "1", "9"],
+    ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+    ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+    ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+    ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+    ["4", "3", "-", "-", "2", "-", "5", "-", "1"],
+    ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+  ]
+
+  it "returns an array" do
+    expect(string_to_board(string)).to eq([["1", "-", "-", "1", "-", "-", "1", "-", "-"], ["1", "-", "-", "1", "-", "-", "1", "-", "-"], ["1", "-", "-", "1", "-", "-", "1", "-", "-"]])
+  end
+
+  it "returns an array of a full board" do
+    expect(string_to_board(unsolved_puzzle)).to eq(correct_board)
+  end
+end
+
+describe "board_to_string" do
+  unsolved_puzzle = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+  correct_board = [
+    ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+    ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+    ["2", "-", "-", "4", "-", "-", "8", "1", "9"],
+    ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+    ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+    ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+    ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+    ["4", "3", "-", "-", "2", "-", "5", "-", "1"],
+    ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+  ]
+  it "returns a string" do
+    expect(board_to_string(correct_board)).to eq(unsolved_puzzle)
+  end
+end
+
+describe "make row" do
+  correct_board = [
+    ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+    ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+    ["2", "-", "-", "4", "-", "-", "8", "1", "9"],
+    ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+    ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+    ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+    ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+    ["4", "3", "-", "-", "2", "-", "5", "-", "1"],
+    ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+  ]
+
+  it "returns a single row from board" do
+    expect(make_row(correct_board, 0)).to eq(correct_board[0])
+  end
+end
+
+describe "make column" do
+  correct_board = [
+    ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+    ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+    ["2", "-", "-", "4", "-", "-", "8", "1", "9"],
+    ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+    ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+    ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+    ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+    ["4", "3", "-", "-", "2", "-", "5", "-", "1"],
+    ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+  ]
+  column = ["1","-","2","-","7","-","-","4","6"]
+
+  it "returns a single column from board" do
+    expect(make_column(correct_board, 0)).to eq(column)
+  end
+end
+
+describe "identify and make square" do
+  correct_board = [
+    ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+    ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+    ["2", "-", "-", "4", "-", "-", "8", "1", "9"],
+    ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+    ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+    ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+    ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+    ["4", "3", "-", "-", "2", "-", "5", "-", "1"],
+    ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+  ]
+  correct_square = ["8", "-", "2","-", "7", "6","4", "-", "-"]
+  correct_square_7 = ["6", "-", "-","-", "2", "-","3", "-", "8"]
+  it "returns a correct square (1)" do
+    expect(identify_and_make_square(correct_board,1)).to eq(correct_square)
+  end
+
+    it "returns a correct square(7)" do
+    expect(identify_and_make_square(correct_board,7)).to eq(correct_square_7)
+  end
+end
+
+describe "square range" do
+  correct_board = [
+    ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+    ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+    ["2", "-", "-", "4", "-", "-", "8", "1", "9"],
+    ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+    ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+    ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+    ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+    ["4", "3", "-", "-", "2", "-", "5", "-", "1"],
+    ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+  ]
+
+  it "returns a correct square from a pair of coordinates" do
+    expect(square_range(4,4)).to eq(4)
+  end
+
+  it "returns a correct square from a second pair of coordinates" do
+    expect(square_range(7,2)).to eq(6)
+  end
+end
+
+describe "array of possible values" do
+  test_array = ["-", "9", "-", "-", "7", "6", "4", "-", "5"]
+  return_array = ["1","2","3","8"]
+  test_array_2 = ["-", "9", "-", "1", "7", "6", "_", "-", "5"]
+  return_array_2 = ["2","3","4","8"]
+
+  it "returns an array with the missing values (from 1-9) from an array" do
+    expect(possible_values(test_array)).to eq(return_array)
+  end
+
+  it "returns an array with the missing values (from 1-9) from an array" do
+    expect(possible_values(test_array_2)).to eq(return_array_2)
+  end
+
+end
+
+describe "check cell" do
+  correct_board = [
+    ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+    ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+    ["2", "7", "6", "4", "-", "-", "8", "1", "9"],
+    ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+    ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+    ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+    ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+    ["4", "3", "8", "-", "2", "-", "5", "-", "1"],
+    ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+  ]
+
+  it "returns a cell's correct number" do
+    expect(check_cell(correct_board,1,2)).to eq("3")
+  end
+
+  it "returns false" do
+    expect(check_cell(correct_board,0,8)).to eq(false)
+  end
+
+end
+
+describe "replace value" do
+    correct_board = [
+    ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+    ["-", "9", "-", "-", "7", "6", "4", "-", "5"],
+    ["2", "7", "6", "4", "-", "-", "8", "1", "9"],
+    ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+    ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+    ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+    ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+    ["4", "3", "8", "-", "2", "-", "5", "-", "1"],
+    ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+  ]
+  it "returns the board with a replaced value for an empty space" do
+    expect(replace_value(correct_board, 1, 2, check_cell(correct_board, 1, 2))).to eq([
+    ["1", "-", "5", "8", "-", "2", "-", "-", "-"],
+    ["-", "9", "3", "-", "7", "6", "4", "-", "5"],
+    ["2", "7", "6", "4", "-", "-", "8", "1", "9"],
+    ["-", "1", "9", "-", "-", "7", "3", "-", "6"],
+    ["7", "6", "2", "-", "8", "3", "-", "9", "-"],
+    ["-", "-", "-", "-", "6", "1", "-", "5", "-"],
+    ["-", "-", "7", "6", "-", "-", "-", "3", "-"],
+    ["4", "3", "8", "-", "2", "-", "5", "-", "1"],
+    ["6", "-", "-", "3", "-", "8", "9", "-", "-"]
+  ])
+  end
+
+  it "Returns the board originally passed in if the value at the current cell is set to false" do
+    expect((replace_value(correct_board, 1, 2, check_cell(correct_board, 0, 8)))).to eq(correct_board)
+  end
+end
