008d69fc3e054d774b6c3f0de422b8ebadf34fa3
Mason Pierce <email": null>
2016-01-02 12:46:31 -0600

    Populate beautiful Prince sudoku solve

--- sudoku/sudoku_spec.rb
+++ sudoku/sudoku_spec.rb
@@ -1,2 +1,8 @@
 require_relative 'sudoku'
 
+describe 'solve' do
+  let (:board){"1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"}
+  it 'returns the solved puzzle for an unsolved puzzle' do
+    expect(solve(board)).to eq("145892673893176425276435819519247386762583194384961752957614238438729561621358947")
+  end
+end
